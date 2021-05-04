576088587570e2ef6b423ad7fb26f95a79bc315e
Domovski1 <624273242@mail.ru>
2021-05-05 01:48:53 +0300

    Добавлены кнопки

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
 {
@@ -25,8 +27,21 @@ namespace TelegramBot
                 Console.WriteLine($"Пришло сообщение: {msg.Text}");
 
                 // Первый аргумент указывает, куда отправлять ответ, а второй аргумент - что отвечать.
-                await client.SendTextMessageAsync(msg.Chat.Id, "Ваше сообщение принято");
+                await client.SendTextMessageAsync(msg.Chat.Id, "Ваше сообщение принято", replyMarkup: GetButtons());
             }
+        }
+
+        private static IReplyMarkup GetButtons()
+        {
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List<KeyboardButton>>
+                {
+                    new List<KeyboardButton> { new KeyboardButton { Text = "TestButton" }, new KeyboardButton { Text = "Вторая кнопка" } },
+                    new List<KeyboardButton> { new KeyboardButton { Text = "TestButton 2" }, new KeyboardButton { Text = "double btn" } },
+
+                }
+            };
         }
     }
 }
