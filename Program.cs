c2dbc401339c2dfa3b0cce7d58f6bf69e20e768f
Domovski1 <624273242@mail.ru>
2021-09-22 22:59:54 +0300

    Всего одна функция - гет видео с ютуба и ретурн

--- Program.cs
+++ Program.cs
@@ -3,19 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using Telegram.Bot;
 using Telegram.Bot.Args;
-using Telegram.Bot.Types.ReplyMarkups;
 using TelegramBot.Classes;
 
 namespace TelegramBot
 {
     class Program
     {
+        /// <summary>
+        /// API Token для бота
+        /// </summary>
         private static string token { get; set; } = "1777573276:AAE7ePm8ZTn4rSVjq9HkNNI27eNf3eFPyCo";
-        private static string path = Environment.CurrentDirectory + @"/" + "TelegramBot.txt";
+
+        /// <summary>
+        /// Ссылка на документ, в котором хранятся все пользователи
+        /// </summary>
+        private static string Path = Environment.CurrentDirectory + @"/" + "TelegramBot.txt";
+        private static string log = Environment.CurrentDirectory + @"/" + "old TelegramBot.txt";
+
+        /// <summary>
+        /// Клиент, для работы с командами
+        /// </summary>
         private static TelegramBotClient client;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Bot is running...");
+            Console.WriteLine("Bot is running....");
             client = new TelegramBotClient(token);
             client.StartReceiving();
             client.OnMessage += OnMessageHandler;
@@ -24,82 +36,96 @@ namespace TelegramBot
             
         }
 
+
+        /// <summary>
+        /// Обработчик сообщений
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private static async void OnMessageHandler(object sender, MessageEventArgs e)   
         {
-            var msg = e.Message;
-            if (msg.Text != null)
+            var MessageFromUser = e.Message;
+
+            // Проверка пользователя, является ли он новым, если да, то его в список юзеров, нет - логи
+            if (!IsUserRegistred(MessageFromUser.From.Id))
             {
-                Console.WriteLine($" {msg.From}: {msg.Text}");
-                StreamWriter writer = new StreamWriter(path, true);
-                writer.WriteLine($"{msg.From}: {msg.Text} ({DateTime.Now})" );
+                Console.WriteLine($"[INFO] Новый пользователь. Имя {MessageFromUser.From}: \nlink: {MessageFromUser.Text}");
+                StreamWriter writer = new StreamWriter(Path, true);
+                writer.WriteLine($"{MessageFromUser.From}: ({DateTime.Now})");
                 writer.Close();
-                
+            } else
+            {
+                Console.WriteLine($"{MessageFromUser.From}: {MessageFromUser.Text}");
+                StreamWriter writer = new StreamWriter(log, true);
+                writer.WriteLine($"{ MessageFromUser.From}: {MessageFromUser.Text} ({DateTime.Now}");
+                writer.Close();
+            }
 
-                switch (msg.Text)
+
+
+            try
+            {
+                // Информация о боте
+                if (MessageFromUser.Text.ToLower().Contains("\\start") || MessageFromUser.Text.ToLower().Contains("\\info"))
                 {
-                    case "Стикер":
-                        var stick = await client.SendStickerAsync(
-                            chatId: msg.Chat.Id,
-                            sticker: "https://cdn.tlgrm.ru/stickers/b48/7e2/b487e222-21cd-4741-b567-74b25f44b21a/192/3.webp",
-                            replyToMessageId: msg.MessageId,
-                            replyMarkup: GetButtons());
-                        break;
-
-                    case "Картинка":
-                        var pic = await client.SendPhotoAsync(
-                            chatId: msg.Chat.Id,
-                            photo: "https://sun9-57.userapi.com/impg/4b7GLehH0kM2PkUP53sxOvf7d09pe49DedXAhQ/nOJWRVm7z_Q.jpg?size=1953x1255&quality=96&sign=14aee579f9e7b078f24e9301c69b75df&type=album",
-                            replyToMessageId: msg.MessageId,
-                            replyMarkup: GetButtons());
-                        break;
-
-                    case "Видео":
+                    await client.SendTextMessageAsync(MessageFromUser.Chat.Id, "Я являюсь загрузчиком видео с Youtube. \nВам нужно лишь отправить мне ссылку на желаемое видео и я скачаю его для вас");
+                }
+                // Основной код
+                else
+                {
+                    try
+                    {
+                        VideoFromYoutubeToAudio.SaveVideoToDisk(MessageFromUser.Text);
+                        Console.WriteLine($"[INFO] Video downloaded \nlink: {MessageFromUser.Text}");
 
                         var vid = await client.SendVideoAsync(
-                            chatId: msg.Chat.Id,
-                            video: File.OpenRead(Environment.CurrentDirectory + @"/" + "vid.MP4"),
-                            thumb: "https://sun9-57.userapi.com/impg/4b7GLehH0kM2PkUP53sxOvf7d09pe49DedXAhQ/nOJWRVm7z_Q.jpg?size=1953x1255&quality=96&sign=14aee579f9e7b078f24e9301c69b75df&type=album",
-                            replyToMessageId: msg.MessageId,
-                            supportsStreaming: true) ;
-                        break;
-
-                    case "Видео-ответ":
-                        using (var stream = File.OpenRead(@"G:\Media\SomeVids\BXAJ0401.MP4"))
-                        {
-                            msg = await client.SendVideoNoteAsync(
-                                chatId: msg.Chat.Id,
-                                videoNote: stream,
-                                duration: 30,
-                                length: 180
-                                );
-                        }
-                        break;
-
-                    default:    
-                        await client.SendTextMessageAsync(msg.Chat.Id, SomeFeatures.PasswordGenerator());
-                        
-                        break;
+                                chatId: MessageFromUser.Chat.Id,
+                                video: File.OpenRead(VideoFromYoutubeToAudio.FileName),
+                                thumb: "https://sun9-57.userapi.com/impg/4b7GLehH0kM2PkUP53sxOvf7d09pe49DedXAhQ/nOJWRVm7z_Q.jpg?size=1953x1255&quality=96&sign=14aee579f9e7b078f24e9301c69b75df&type=album",
+                                replyToMessageId: MessageFromUser.MessageId,
+                                supportsStreaming: true);
+                    }
+                    catch (Exception ex)
+                    {
+                        await client.SendTextMessageAsync(MessageFromUser.Chat.Id, "Упс, что-то пошло не так.. \nВозможно вы ввели неверную команду \nМожно вызвать информацию через команду \\info");
+                        Console.WriteLine(ex.Message);
+                    }
                 }
-
-
-                // Первый аргумент указывает, куда отправлять ответ, а второй аргумент - что отвечать.
-                //await client.SendTextMessageAsync(msg.Chat.Id, "Ваше сообщение принято", replyMarkup: GetButtons());
             }
-        }
-
-
-        private static IReplyMarkup GetButtons()
-        {
-            return new ReplyKeyboardMarkup
+            catch (Exception ex)
             {
-                Keyboard = new List<List<KeyboardButton>>
-                {
-                    new List<KeyboardButton> { new KeyboardButton { Text = "Видео" }, new KeyboardButton { Text = "Видео-ответ" }},
-                    new List<KeyboardButton> { new KeyboardButton { Text = "Стикер" }, new KeyboardButton { Text = "Картинка" } }
-
-                }
-            };
+                Console.WriteLine("[INFO] " + ex);
+            }
+            
         }
+        
+
+
+        /// <summary>
+        /// Проверка наличия Id в базе
+        /// </summary>
+        /// <param name="UserId">ID пользователя</param>
+        /// <returns></returns>
+        private static bool IsUserRegistred(int UserId)
+        {
+            List<string> Clietns = new List<string>();
+
+            var doc = File.ReadAllLines(Path);
+
+            foreach (var item in doc)
+            {
+                if (item.Contains($"{UserId.ToString()}"))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        
     }
 }
 
+// Можно доработать (конверт то мпз, гет фром инст)
